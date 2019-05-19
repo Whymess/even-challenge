926b6a5b95475a3bea4d4db434071b8dfacfc1f9
Whymess <Jonathan.lev.messing@gmail.com>
2019-05-19 15:57:45 -0500

    required packeges added

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -1,13 +1,20 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 //local dependencies
 import "./index.css";
 
-export default props => {
+const Footer = props => {
 	let { title } = props;
 	return (
 		<footer className="footer mt-4 ml-auto">
 			<p className="footer-text">© {title}</p>
 		</footer>
 	);
+};
+
+export default Footer;
+
+Footer.propTypes = {
+	title: PropTypes.string.isRequired
 };
